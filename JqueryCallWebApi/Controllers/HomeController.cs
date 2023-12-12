5b4cd456ea6f266f2a5a56486c9d2ca55dcdf1a0
BRIGHT <wissen30@hotmail.com>
2023-12-12 20:52:38 +0300

    Add project files.

---
+++ JqueryCallWebApi/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JqueryCallWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("JqueryGet")]
+        public IActionResult Index()
+        {
+            return Ok(true);
+        }
+        [HttpPost]
+        [Route("AddProduct")]
+        public IActionResult Post([FromBody]Product product)
+        {
+            return Ok(product == null ? false : true);
+        }
+    }
+}
---
+++ JqueryCallWebApi/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace JqueryCallWebApi
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Category { get; set; }
+    }
+}
